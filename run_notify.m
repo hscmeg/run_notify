52fac584187eb3d3bae1a06f97d7b8f184864abc
Simeon Wong <me@simeonwong.com>
2020-06-17 11:41:18 -0400

    also show runtime in hours

--- run_notify.m
+++ run_notify.m
@@ -14,6 +14,8 @@ end
 cname = gethostname;
 tElapsed = tic;
 
+fprintf('%s started at %.\n', script_name, datestr(now));
+
 try
     evalin('base', script_name);
     tElapsed = toc(tElapsed);
@@ -24,11 +26,11 @@ catch exception
     tElapsed = toc(tElapsed);
     
     title = ['Execution Error on ', cname];
-    message = sprintf('Error executing %s!\n---\n%s\n---\nTime Elapsed: %.1fs', script_name, exception.message, tElapsed);
+    message = sprintf('%s execution error!\n---\n%s\n---\nTime Elapsed: %.1fs', script_name, exception.message, tElapsed);
     cprintf('red', getReport(exception));
 end
 
-cprintf('blue', '\n---\nExecution Complete: %s\nTime Elapsed: %.1fs\n---\n', script_name, tElapsed);
+cprintf('blue', '\n---\n%s execution completed at %s\nTime Elapsed: %.1fs\t(%.2fh)\n---\n', script_name, datestr(now), tElapsed, tElapsed / 3600);
 
 if exist('API_TOKEN', 'var')
     post_params = {'token', API_TOKEN,...    % API token
