f73f60f294f26727702f9c55db3d7860e0c255c8
Simeon Wong <me@simeonwong.com>
2020-06-18 17:24:50 -0400

    remove external dependencies

--- notify.m
+++ notify.m
@@ -4,7 +4,7 @@ function [  ] = notify( message )
 % Sends an alert using pushover to the specified device in pushover_parameters.mat
 
 if exist('pushover_parameters.mat', 'file')
-    load pushover_parameters.mat
+    load('pushover_parameters.mat', 'API_TOKEN', 'USER_TOKEN');
     
     cname = gethostname;
     title = ['Message from ', cname];
@@ -14,11 +14,11 @@ if exist('pushover_parameters.mat', 'file')
                    'message', message,...       % message to push
                    'title', title};             % message title in notification bar
            
-    urlread('https://api.pushover.net/1/messages.json', 'Post', post_params);
+    webwrite('https://api.pushover.net/1/messages.json', post_params{:});
 end
 
 % Display local message
-cprintf('blue', [message, '\n']);
+fprintf([message, '\n']);
 
 end
 
--- run_notify.m
+++ run_notify.m
@@ -1,44 +1,77 @@
-function [  ] = run_notify( script_name )
+function [  ] = run_notify( script_name, varargin )
 %run_notify
 %
 % Runs the specified script, and sends a Pushover notification to the given
 % user token after completion, or upon an error.
 
+ip = inputParser;
+addOptional(ip, 'DiaryDir', []);
+
+parse(ip, varargin{:});
+
 %% Parameters
+cname = getenv('VNCDESKTOP');
+tElapsed = tic;
+
 if exist('pushover_parameters.mat', 'file')
-    load pushover_parameters.mat
+  pparam = load('pushover_parameters.mat');
+end
+
+if ~isempty(ip.Results.DiaryDir)
+  diarypath = ip.Results.DiaryDir;
+elseif isfield(pparam, 'DIARY_DIR')
+  diarypath = pparam.DIARY_DIR;
+else
+  diarypath = [];
+end
+
+
+if ~isempty(diarypath)
+  diarypath = fullfile(diarypath, datestr(now, 'yyyymmdd'));
+  
+  if ~exist(diarypath, 'file')
+    mkdir(diarypath);
+  end
+  
+  sysinfo = regexprep(regexprep(cname, '[\(\)]',''), '[^a-zA-Z0-9-_.]', '_');
+  diarypath = fullfile(diarypath, sprintf('%s_%s_%s.txt', datestr(now, 'HHMMSS'), sysinfo, script_name));
+  
+  diary(diarypath);
 end
 
 %% Script
+start_str = datestr(now);
+fprintf('%s started at %s on %s\n\n', script_name, start_str, getenv('VNCDESKTOP'));
 
-cname = gethostname;
-tElapsed = tic;
-
-fprintf('%s started at %.\n', script_name, datestr(now));
 
 try
-    evalin('base', script_name);
-    tElapsed = toc(tElapsed);
-    
-    title = ['Execution Success on ', cname];
-    message = sprintf('Execution success: %s\nTime Elapsed: %.1fs', script_name, tElapsed);
+  evalin('base', script_name);
+  tElapsed = toc(tElapsed);
+  
+  title = ['Execution Success on ', cname];
+  message = sprintf('Execution success: %s\nTime Elapsed: %.1fs', script_name, tElapsed);
 catch exception
-    tElapsed = toc(tElapsed);
-    
-    title = ['Execution Error on ', cname];
-    message = sprintf('%s execution error!\n---\n%s\n---\nTime Elapsed: %.1fs', script_name, exception.message, tElapsed);
-    cprintf('red', getReport(exception));
+  tElapsed = toc(tElapsed);
+  
+  title = ['Execution Error on ', cname];
+  message = sprintf('%s execution error!\n---\n%s\n---\nTime Elapsed: %.1fs', script_name, exception.message, tElapsed);
+  fprintf(getReport(exception));
 end
 
-cprintf('blue', '\n---\n%s execution completed at %s\nTime Elapsed: %.1fs\t(%.2fh)\n---\n', script_name, datestr(now), tElapsed, tElapsed / 3600);
+fprintf('\n---\n%s execution from %s to %s.\nTime Elapsed: %.1fs (%.2fh)\n---\n', ...
+  script_name, start_str, datestr(now), tElapsed, tElapsed / 3600);
 
-if exist('API_TOKEN', 'var')
-    post_params = {'token', API_TOKEN,...    % API token
-                   'user', USER_TOKEN,...    % user's ID token
-                   'message', message,...    % message to push
-                   'title', title};          % message title in notification bar
+if ~isempty(diarypath)
+  diary('off');
+end
 
-    urlread('https://api.pushover.net/1/messages.json', 'Post', post_params);
+if exist('pparam', 'var')
+  post_params = {'token', pparam.API_TOKEN,...    % API token
+    'user', pparam.USER_TOKEN,...    % user's ID token
+    'message', message,...    % message to push
+    'title', title};          % message title in notification bar
+  
+  webwrite('https://api.pushover.net/1/messages.json', post_params{:});
 end
 
 end
